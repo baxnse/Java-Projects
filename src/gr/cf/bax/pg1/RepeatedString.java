8236c1a8e6c02de1e4f9ddece0fe8327d1c0e0f9
baxnse <baxn.se@gmail.com>
2025-07-31 15:55:46 +0300

    feat: add ReapeatedString class

---
+++ src/gr/cf/bax/pg1/RepeatedString.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch10_challenges;
+
+public class RepeatedString {
+
+    public static void main(String[] args) {
+        String s = "amalia";
+        System.out.println(multiplyString(s, 3));
+        System.out.println(getRepeated2(s, 10));
+    }
+
+    public static String multiplyString (String s, int a) {
+
+        return s.repeat(a);
+    }
+
+    public static String getRepeated2(String s, int n) {
+        int times;
+
+        times = (int) Math.ceil((double) n / s.length());
+        return s.repeat(times);
+    }
+}
