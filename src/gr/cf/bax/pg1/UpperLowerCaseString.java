623c5d252321786911215cf36ac435ca181e6ac3
baxnse <baxn.se@gmail.com>
2025-08-04 11:16:47 +0300

    feat: update method

--- src/gr/cf/bax/pg1/UpperLowerCaseString.java
+++ src/gr/cf/bax/pg1/UpperLowerCaseString.java
@@ -8,36 +8,26 @@ public class UpperLowerCaseString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insert the word you want to format");
+        System.out.println("Please input a word for case formatting:");
         String s = scanner.nextLine();
 
         formatString(s);
     }
 
     public static void formatString(String s) {
-        String str1 = "";
-        String str2 = "";
-        String str3 = "";
-        String str4 = "";
-        String str5 = "";
 
-        for (int i = 0; i < s.length(); i = i + 2) {
-            str1 = String.valueOf(s.charAt(i) + " ");
-            str2 += str1.toUpperCase();
+        String lowerS = s.toLowerCase();
+        StringBuilder finalS = new StringBuilder(lowerS.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = lowerS.charAt(i);
+
+            if ( i % 2 == 0) {
+                finalS.append(Character.toUpperCase(c));
+            }
+            else finalS.append(c);
         }
 
-        for (int i = 1; i < s.length(); i = i + 2) {
-            str3 = String.valueOf(" " + s.charAt(i));
-            str4 += str3.toLowerCase();
-        }
-        for (int i = 0; i < str4.length(); i++) {
-
-        str5 = str2.replace(" ",String.valueOf(str4.charAt(i))); // BUG!!!!
-
-        }
-
-        System.out.println(str2);
-        System.out.println(str4);
-        System.out.println(str5);
+        System.out.println(finalS);
     }
 }
