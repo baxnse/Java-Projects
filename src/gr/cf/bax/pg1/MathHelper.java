da9dc90ec0e3d5c61dee76eab83d63987e34dbf9
baxnse <baxn.se@gmail.com>
2025-08-14 11:37:22 +0300

    "feat: add MathHelper utility class"

---
+++ src/gr/cf/bax/pg1/MathHelper.java
@@ -0,0 +1,36 @@
+package gr.cf.bax.pg1;
+
+public class MathHelper {
+
+    private MathHelper() {
+
+    }
+
+    public static int maxValue(int[] arr) {
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if ( arr[i] > max) max = arr[i];
+        }
+        return max;
+    }
+
+    public static int minValue(int[] arr) {
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if ( arr[i] < min) min = arr[i];
+        }
+        return min;
+    }
+
+    public static double averageValue(int[] arr) {
+        int num = 0;
+
+        for (int el : arr) {
+            num += el;
+
+        }
+        return (double) num / arr.length;
+    }
+}
